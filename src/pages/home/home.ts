47fa8efcde3f33e35b82b0832b70a881731e7e50
leoalcantara <leonidio.ti@gmail.com>
2018-09-15 19:48:24 -0300

    designer buttons

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,14 +2,22 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {FormBuilder, Validators} from '@angular/forms';
 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public valuesForm: any;
+
+
+
+  constructor(formBuilder: FormBuilder){
+    this.valuesForm = formBuilder.group({
+      Gasolina:['', Validators.required],
+      Etanol:['', Validators.required]
+    })
 
   }
-
 }
