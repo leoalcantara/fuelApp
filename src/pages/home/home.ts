9450f8da85d871bc4681c54287d1274b0a9a94d3
leoalcantara <leonidio.ti@gmail.com>
2018-09-18 22:04:32 -0300

    form validation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,24 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NavController, ModalController } from 'ionic-angular';
-import {FormBuilder, Validators} from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
 
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
-
+  
   public valuesForm: any;
+  erroGasolina = false;
+  erroEtanol = false;
+  messageGasolina = '';
+  messageEtanol = '';
 
-  public  fuel(gasolina: number, etanol:number):string{   
-    if(( gasolina * 0.70) >= etanol ){
-      return "Etanol";
+
+  constructor(formBuilder: FormBuilder) {
+    this.valuesForm = formBuilder.group({
+      gasolina: ['', Validators.required],
+      etanol: [' ', Validators.required],
+    })
+  }
+ 
+  compara(){
+    let {gasolina,etanol} = this.valuesForm.controls;
+    if(!this.valuesForm.valid){
+       if(!gasolina.valid){
+         this.erroGasolina = true;
+         this.messageGasolina = 'Ops! valor invalido.'          
+       } else {
+         this.messageGasolina = '';
+         this.erroGasolina = false;
+       }
+
+       if (!etanol.valid) {
+         this.erroEtanol = true;
+         this.messageEtanol = 'Ops! valor invalido.' 
+         
+       } else {
+         this.messageEtanol = '';
+         this.erroEtanol = false;
+       }
     } else {
-      return "Gasolina";
+      //alert("OK");
+      this.erroEtanol = false;
+      this.erroGasolina = false;
     }
 
-  };
+    
+  }
+
+}
 
   
-}
+
