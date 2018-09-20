70273deecea62787276664be46c9a29236a8a868
leoalcantara <leonidio.ti@gmail.com>
2018-09-20 19:01:58 -0300

    Fuel comparison feature

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
-import { NavController, ModalController } from 'ionic-angular';
-import { FormBuilder, Validators } from '@angular/forms';
+import { NavController, ModalController, AlertController } from 'ionic-angular';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
 
 
@@ -10,47 +10,55 @@ import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_di
 })
 
 export class HomePage {
-  
-  public valuesForm: any;
+  valuesForm: FormGroup;
   erroGasolina = false;
   erroEtanol = false;
   messageGasolina = '';
   messageEtanol = '';
 
 
-  constructor(formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder) {
     this.valuesForm = formBuilder.group({
       gasolina: ['', Validators.required],
       etanol: [' ', Validators.required],
     })
   }
- 
-  compara(){
-    let {gasolina,etanol} = this.valuesForm.controls;
-    if(!this.valuesForm.valid){
-       if(!gasolina.valid){
-         this.erroGasolina = true;
-         this.messageGasolina = 'Ops! valor invalido.'          
-       } else {
-         this.messageGasolina = '';
-         this.erroGasolina = false;
-       }
 
-       if (!etanol.valid) {
-         this.erroEtanol = true;
-         this.messageEtanol = 'Ops! valor invalido.' 
-         
-       } else {
-         this.messageEtanol = '';
-         this.erroEtanol = false;
-       }
+  validar() {
+    let { gasolina, etanol } = this.valuesForm.controls;
+    if (!this.valuesForm.valid) {
+      if (!gasolina.valid) {
+        this.erroGasolina = true;
+        this.messageGasolina = 'Ops! Preencha o campo Gasolina '
+      } else {
+        this.messageGasolina = '';
+        this.erroGasolina = false;
+      }
+
+      if (!etanol.valid) {
+        this.erroEtanol = true;
+        this.messageEtanol = 'Ops! Preencha campo Etanol '
+
+      } else {
+        this.messageEtanol = '';
+        this.erroEtanol = false;
+      }
     } else {
-      //alert("OK");
+      //alert(parseFloat(gasolina.value) + parseFloat(etanol.value));
       this.erroEtanol = false;
       this.erroGasolina = false;
+      alert(this.comparar(gasolina, etanol));
     }
 
-    
+  }
+
+  comparar(gasolina, etanol): string{
+    if((parseFloat(gasolina.value)*0.7) > parseFloat(etanol.value)){
+      return "Etanol"
+    } else{
+      return "Gasolina"
+    }
+
   }
 
 }
